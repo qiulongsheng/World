a1c502080f6ea38a0c0a29da1009b5e0ac19c12a
qiulongsheng <569950292@qq.com>
2017-07-10 23:22:22 +0800

    no message

--- Scrapy爬虫/运行爬虫.py
+++ Scrapy爬虫/运行爬虫.py
@@ -15,4 +15,4 @@ import subprocess
 spiderdir = raw_input('输入爬虫项目的完整路径(如 C:\zhaoshang): '.encode('mbcs'))
 spidername = raw_input('输入爬虫名称(如zhaoshang): '.encode('mbcs'))
 os.chdir(spiderdir)
-subprocess.call('scrapy crawl %s -o %s.csv' % ('a' + spidername,spidername))
+subprocess.call('scrapy crawl %s -o %s.csv' % ('a' + spidername, spidername))
