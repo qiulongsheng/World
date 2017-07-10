8c1c3746a729176cb69cc9535d5df8d0b8631cc9
Atlantis <569950292@qq.com>
2017-07-10 23:20:05 +0800

    Update 创建爬虫.py

--- Scrapy爬虫/创建爬虫.py
+++ Scrapy爬虫/创建爬虫.py
@@ -16,6 +16,6 @@ os.chdir('C:\\')
 name = raw_input('输入爬虫项目名称(如 zhaoshang): '.encode('mbcs'))
 a = 'scrapy startproject ' + name
 subprocess.call(a)
-os.chdir(os.path.join(os.getcwd(),name))
+os.chdir(os.path.join(os.getcwd(), name))
 domain = raw_input('输入理财产品网址域名(如 cmbchina.com): '.encode('mbcs'))
-subprocess.call('scrapy genspider a'+name+' '+domain)
+subprocess.call('scrapy genspider a' + name + ' ' + domain)
